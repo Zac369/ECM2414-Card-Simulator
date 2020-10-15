9dbd2abe6f1160d50471aadb489505c263b8a4e6
pierre <dumboyadvance@gmail.com>
2020-10-15 10:37:58 +0100

    Discard, draw

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,3 +1,35 @@
+import java.util.LinkedList;
+
 public class CardDeck {
-    //this class is not required but recommended
+    //Reusing materials from W3 CW.
+    LinkedList<Integer> deck = new LinkedList<>();
+    int capacity = 4;
+    // Function called by producer thread
+
+/*
+    public synchronized void discard(int cardIndex, CardDeck destinationDeck){
+        int discardedCard = deck.get(cardIndex);
+        deck.remove(cardIndex);
+        destinationDeck.deck.addLast(discardedCard);
+        notify();
+
+    }
+
+
+
+    // Function called by consumer thread
+    public synchronized void consume() throws InterruptedException {
+        for (int i = 0; i < 20; ) {
+            if (!deck.isEmpty()) {
+                deck.remove();
+                System.out.println(deck.toString());
+                i++;
+                notify();
+            } else {
+                wait();
+            }
+        }
+
+
+//*/
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,30 @@
-public class Player {
+import java.util.Random;
+
+public class Player extends CardDeck {
+    Random r = new Random();
+    int preferredDenomination = r.nextInt(13);
+
+    /**
+     * @param cardIndex       0 <= int < 4
+     * @param destinationDeck CardDeck to discard to
+     */
+    public synchronized void discard(int cardIndex, CardDeck destinationDeck) {
+        int discardedCard = deck.get(cardIndex);
+        deck.remove(cardIndex);
+        destinationDeck.deck.addLast(discardedCard);
+        notify();
+    }
+
+    /**
+     *
+     * @param sourceDeck CardDeck to draw from
+     */
+    public synchronized void draw(CardDeck sourceDeck) {
+        deck.addFirst(sourceDeck.deck.poll());
+    }
+
+    //Strategy
+
+
+
 }
