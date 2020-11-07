2be807ea321828ae7365ff0082807d052821ffb8
DESKTOP-OUEH4R0\willi <williamszac20@gmail.com>
2020-11-07 13:18:24 +0000

    Implement cli questions
    Treat card draw and discard as a single atomic action
    
    Cards can only be positive
    
    Players output game commands to file

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,15 +1,47 @@
 import javax.sound.midi.SysexMessage;
 import javax.sql.rowset.CachedRowSet;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class CardGame {
     public static void main(String[] args) throws InterruptedException {
         int numberOfPlayers = 4;
         String filePath = "C:\\Users\\willi\\IdeaProjects\\ECM2414Coursework\\src\\pack.txt";
 
+        Scanner scanner = new Scanner(System.in);
+        /*
+
+        int numberOfPlayers = 0;
+
+        boolean done = false;
+        while (!done) {
+            try {
+                System.out.println("Number of players:");
+                numberOfPlayers = Integer.parseInt(scanner.nextLine());
+                if (numberOfPlayers > 1) {
+                    done = true;
+                } else {
+                    System.out.println("That number is not greater than 1. Please input a number greater than 1");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("That's not a valid number. Please input a valid number that is greater than 1.");
+            }
+        }
+
+        System.out.println("Enter filepath:");
+        String filePath = scanner.nextLine();*/
+
+
+
 
         Pack test = new Pack();
 
-        test.validatePack(numberOfPlayers, filePath);
+        while (!(test.validatePack(numberOfPlayers, filePath))) {
+            System.out.println("Please enter a valid file path to a valid pack file");
+            filePath = scanner.nextLine();
+           /* System.out.println("Number of players:");
+            numberOfPlayers = Integer.parseInt(scanner.nextLine());*/
+        }
 
         CardDeck[] cardDeckList = new CardDeck[numberOfPlayers];
         for (int i = 0; i < numberOfPlayers; i++) {
--- src/Player.java
+++ src/Player.java
@@ -1,33 +1,30 @@
-import jdk.jfr.StackTrace;
-
-import javax.crypto.NullCipher;
 import java.util.LinkedList;
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter;
 
 public class Player extends CardDeck {
     Thread thread = new Thread(new Runnable() {
         @Override
         public void run() {
+            writeToFile(playerOutputPathName, "Player " + playerID + " initial hand " + deck.toString() + "\n");
             notIndexCards = (LinkedList) deck.clone();
             while (notIndexCards.contains(playerID)) {
                 notIndexCards.removeFirstOccurrence(playerID);
             }
             while (!isVictory()) {
                 //Strategy
-                draw(cardDeckList[index]);
-                System.out.println("This is a turn from Player "+ playerID + deck.toString() + "Deck to my left " +cardDeckList[index].deck.toString() + "Deck to my right "+cardDeckList[nextIndex].deck.toString());
-                if (deck.getFirst()!= playerID) {
+
+                drawAndDiscard(cardDeckList[index], notIndexCards.peekLast(), cardDeckList[nextIndex]);
+                //if drawn card is not the player's preferred denomination (i.e. their playerID), then add it to notIndexCards
+                if (deck.getFirst() != playerID) {
                     notIndexCards.add(deck.getFirst());
                 }
-                if (isVictory()) {
-                    //Do all the things that break the game
-                }
-                try {
-                    discard(notIndexCards.peekLast(), cardDeckList[nextIndex]);
-                } catch (NullPointerException e) {
-                    //e.printStackTrace();
-                    System.out.println("discard broke");
-                }
-                thread.notify();
+                //System.out.println("Player "+ playerID + "'s cards to be discarded are " +notIndexCards.toString() +
+                //"\nPlayer "+ playerID + "'s current hand is " +deck.toString());
+
+
+                //thread.notify();
                 /*try {
                     thread.wait();
                 } catch (InterruptedException e) {
@@ -35,7 +32,11 @@ public class Player extends CardDeck {
                 }*/
 
             }
-                System.out.println("Hello, I'm Player " + playerID + " and here is my hand" + deck.toString());
+
+            //Victory is achieved
+            //Do all the things that break the game
+            writeToFile(deckOutputPathName, "Deck " +playerID+" content: "+cardDeckList[playerID].deck.toString());
+            System.out.println("Hello, I'm Player " + playerID + " and here is my hand" + deck.toString());
 
 /*            try {
                 thread.wait();
@@ -52,39 +53,46 @@ public class Player extends CardDeck {
     LinkedList<Integer> notIndexCards = new LinkedList<>();
     Player[] playerList;
     CardDeck[] cardDeckList;
+    String playerOutputPathName;
+    String deckOutputPathName;
 
+    /**
+     * Player constructor, helps establish the indexes of players and decks
+     *
+     * @param list  Player array
+     * @param list2 Deck array
+     * @param a     this player's array index (Not ID)
+     */
     public Player(Player[] list, CardDeck[] list2, int a) {
         playerList = list;
         cardDeckList = list2;
         index = a;
         playerID = a + 1;
         nextIndex = index + 1;
-        if (playerID >= playerList.length){
+        if (playerID >= playerList.length) {
             nextIndex = 0;
         }
+        playerOutputPathName = "Player" + playerID + "_output.txt";
+        deckOutputPathName = "Deck" + playerID + "_output.txt";
+        createFile(playerOutputPathName);
+        createFile(deckOutputPathName);
     }
 
 
-
-
-
     /**
-     * @param cardValue       Discarded Card
-     * @param destinationDeck CardDeck to discard to
+     * @param sourceDeck      Deck to draw from
+     * @param cardValue       Value of the card you wish to discard
+     * @param destinationDeck Deck you want to discard to
      */
-    public synchronized void discard(int cardValue, CardDeck destinationDeck) {
+    public synchronized void drawAndDiscard(CardDeck sourceDeck, int cardValue, CardDeck destinationDeck) {
+        int cardAdded = sourceDeck.deck.poll();
         notIndexCards.removeFirstOccurrence(cardValue);
         deck.removeFirstOccurrence(cardValue);
+        deck.addFirst(cardAdded);
         destinationDeck.deck.addLast(cardValue);
-        //notify();
-    }
-
-    /**
-     *
-     * @param sourceDeck CardDeck to draw from
-     */
-    public synchronized void draw(CardDeck sourceDeck) {
-        deck.addFirst(sourceDeck.deck.poll());
+        writeToFile(playerOutputPathName, "Player " + playerID + " draws a " + cardAdded + " from deck " + (index + 1) +
+                "\nPlayer " + playerID + " discards a " + cardValue + " to deck " + (nextIndex + 1) +
+                "\nPlayer " + playerID + "'s current hand is " + deck.toString() + "\n");
     }
 
 
@@ -92,5 +100,38 @@ public class Player extends CardDeck {
         return notIndexCards.size() == 0;
     }
 
+    public boolean createFile(String pathName) {
+        try {
+            File outputFile = new File(pathName);
+            if (outputFile.createNewFile()) {
+                System.out.println("File created: " + outputFile.getName());
+            } else {
+                System.out.println("File already exists.");
+                new FileWriter(pathName).close();
+            }
+            return true;
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void writeToFile(String pathName, String content) {
+        try {
+            FileWriter myWriter = new FileWriter(pathName, true);
+            myWriter.write(content);
+            myWriter.close();
+            //System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 
 }
+
+
+
+
+
